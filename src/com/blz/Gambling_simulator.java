773e12e5b893fc844d62f696117e93301b26803b
LDeepakReddy <l.deepakreddy.com>
2022-02-24 08:29:52 +0530

    UC_1

--- src/com/blz/Gambling_simulator.java
+++ src/com/blz/Gambling_simulator.java
@@ -6,7 +6,7 @@ public class Gambling_simulator {
     public static void main(String[] args) {
         int DAY_STAKE = 100;
         int Bet = 1;
-        System.out.println("initial stake is " + DAY_STAKE + "and bet is " + Bet);
+        System.out.println("initial stake is " + DAY_STAKE + "$  and bet is " + Bet + "$");
 
     }
 }
