79ca78c7c0952c96300e5506150a958edcbb71a0
LDeepakReddy <l.deepakreddy.com>
2022-02-24 09:19:04 +0530

    UC_2

--- src/com/blz/Gambling_simulator.java
+++ src/com/blz/Gambling_simulator.java
@@ -2,11 +2,22 @@ package com.blz;
 
 public class Gambling_simulator {
     public static final int DAY_STAKE = 100;
-    public static final int Bet = 1;
+    public static final int BET = 1;
+
 
     public static void main(String[] args) {
+        int totalCash = DAY_STAKE;
 
-        System.out.println("initial stake is " + DAY_STAKE + "$  and bet is " + Bet + "$");
+        System.out.println("initial stake is " + DAY_STAKE + "$  and bet is " + BET + "$");
+        double random = Math.random();
+        if (random > 0.5) {
+            System.out.println("Bet won");
+            totalCash++;
 
+            System.out.println("Total amount is " + totalCash + "$");
+        } else {
+            System.out.println("Bet loses");
+            totalCash--;
+        }
     }
 }
