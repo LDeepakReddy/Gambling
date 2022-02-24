1d362acc1cf988ffbe8793c18649529c6006d562
LDeepakReddy <l.deepakreddy.com>
2022-02-24 20:32:42 +0530

    Exit when  50% profit or loss

--- src/com/blz/Gambling_simulator.java
+++ src/com/blz/Gambling_simulator.java
@@ -1,23 +1,37 @@
 package com.blz;
 
+
 public class Gambling_simulator {
     public static final int DAY_STAKE = 100;
     public static final int BET = 1;
-
+    public static final int RESIGN_MARGIN = (DAY_STAKE / 100) * 50;
+    public static final int Winning_resign = DAY_STAKE + RESIGN_MARGIN;
+    public static final int Losing_resign = DAY_STAKE - RESIGN_MARGIN;
 
     public static void main(String[] args) {
         int totalCash = DAY_STAKE;
-
         System.out.println("initial stake is " + DAY_STAKE + "$  and bet is " + BET + "$");
-        double random = Math.random();
-        if (random > 0.5) {
-            System.out.println("Bet won");
-            totalCash++;
 
-            System.out.println("Total amount is " + totalCash + "$");
+        while (totalCash < Winning_resign && totalCash > Losing_resign) {
+            int random = (int) (Math.random() * 10) % 2;
+            if (random == 1) {
+                System.out.println("Bet won");
+                totalCash = totalCash + BET;
+                System.out.println("Total amount is " + totalCash + "$");
+            } else {
+                System.out.println("Bet loses");
+                totalCash = totalCash - BET;
+                System.out.println("Remaining amount is " + totalCash + "$");
+            }
+        }
+        if (totalCash == Winning_resign) {
+            System.out.println("Player won by " + totalCash + "$");
         } else {
-            System.out.println("Bet loses");
-            totalCash--;
+            System.out.println("Player lost by " + totalCash + "$");
         }
     }
+
 }
+
+
+
