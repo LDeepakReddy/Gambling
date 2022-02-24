04cb64e24bb86d30341e41ccd51af62a478c3643
LDeepakReddy <l.deepakreddy.com>
2022-02-24 08:37:27 +0530

    Initial stake and bet

--- src/com/blz/Gambling_simulator.java
+++ src/com/blz/Gambling_simulator.java
@@ -1,11 +1,11 @@
 package com.blz;
 
 public class Gambling_simulator {
-
+    public static final int DAY_STAKE = 100;
+    public static final int Bet = 1;
 
     public static void main(String[] args) {
-        int DAY_STAKE = 100;
-        int Bet = 1;
+
         System.out.println("initial stake is " + DAY_STAKE + "$  and bet is " + Bet + "$");
 
     }
